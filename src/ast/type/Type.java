ad81c5677caa81e603fdb74f58089a98ad2df55a
jesQM <jezuquema@gmail.com>
2020-03-29 16:35:55 +0200

    Added basic methods to Type interface

--- src/ast/type/Type.java
+++ src/ast/type/Type.java
@@ -1,7 +1,19 @@
 package ast.type;
 
 import ast.ASTNode;
+import ast.expression.Expression;
+
+import java.util.List;
 
 public interface Type extends ASTNode {
+    Type arithmetic(Type other, ASTNode lineAndColumn);
+    Type squareBrackets(Type other, ASTNode lineAndColumn);
+    Type comparison(Type other, ASTNode lineAndColumn);
+    boolean isBoolean(ASTNode lineAndColumn);
 
+    void canBeAssigned(Type other, ASTNode lineAndColumn);
+    Type not(ASTNode lineAndColumn);
+    void canBeMinus(ASTNode lineAndColumn);
+    void canBeinvoked(List<Expression> parameters, ASTNode lineAndColumn);
+    Type dot(String ID, ASTNode lineAndColumn);
 }
