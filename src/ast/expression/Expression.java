8c67f51e6b301b2d7204fb66de1624fb8c95ab9a
jesQM <jezuquema@gmail.com>
2020-03-29 20:59:21 +0200

    Before starting TypeCheckingVisitor

--- src/ast/expression/Expression.java
+++ src/ast/expression/Expression.java
@@ -1,8 +1,12 @@
 package ast.expression;
 
 import ast.ASTNode;
+import ast.type.Type;
 
 public interface Expression extends ASTNode {
     boolean getLvalue();
     void setLvalue(boolean lvalue);
+
+    Type getType();
+    void setType(Type type);
 }
